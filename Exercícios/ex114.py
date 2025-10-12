69570a9a945d11e4f8a6a3983a8cb4ec9dea7779
Daniel <dan_felixx@hotmail.com>
2025-10-11 21:51:52 -0300

    feat: Finalização do curso mundo 3 de python com todos os 115 exercicios feito!

---
+++ Exercícios/ex114.py
@@ -0,0 +1,11 @@
+import urllib
+import urllib.request
+
+try:
+    site = urllib.request.urlopen('http://www.pudim.com.br')
+except urllib.error.URLError:
+    print('O site pudim não está acessível no momento.')
+else:
+    print('Tudo OK')
+   # print(site.read())
+
